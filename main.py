937dfc9fedf8cb6f2a213f8fcfb2b119d4801d04
scratchedguitar <scratchedguitar@gmail.com>
2021-12-31 23:29:59 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+from gpiozero import LED
+from time import sleep
+from gpiozero import Buzzer
+from gpiozero import TonalBuzzer
+from gpiozero.tones import Tone
+
+b = TonalBuzzer(17)
+
+led = LED(2)
+led.off()
+
+def startTones():
+  b.play(Tone(222.0))
+  sleep(1)
+  b.play(Tone(249.19))
+  sleep(1)
+  b.stop()
+
+def endTones():
+  b.play(Tone(296.00))
+  sleep(0.25)
+  b.stop()
+  b.play(Tone(296.00))
+  sleep(0.25)
+  b.stop()
+
+def dispense(ml):
+  print(ml + "ml dispensing")
+    startTones()
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
+    led.on()
+    sleep(1)
+    led.off()
+    endTones()
+  print(ml + "ml dispense complete")
+
+
+dispense(1000)
