a67f855b7579946d15f14310841f4be47eb07bfb
scratchedguitar <scratchedguitar@gmail.com>
2022-01-01 00:11:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,33 @@ from time import sleep
 from gpiozero import Buzzer
 from gpiozero import TonalBuzzer
 from gpiozero.tones import Tone
+from gpiozero import Button
 
 b = TonalBuzzer(17)
-
 led = LED(2)
 led.off()
+button = Button(3)
+print("Setting up")
+print("Motor  Output on Pin: " + led)
+print("Buzzer Output on Pin: " + b)
+print("Button  Input on Pin: " + button)
+
+
 
 def startTones():
-  b.play(Tone(222.0))
-  sleep(1)
-  b.play(Tone(249.19))
-  sleep(1)
-  b.stop()
+    b.play(Tone(222.0))
+    sleep(1)
+    b.play(Tone(249.19))
+    sleep(1)
+    b.stop()
 
 def endTones():
-  b.play(Tone(296.00))
-  sleep(0.25)
-  b.stop()
-  b.play(Tone(296.00))
-  sleep(0.25)
-  b.stop()
+    b.play(Tone(296.00))
+    sleep(0.25)
+    b.stop()
+    b.play(Tone(296.00))
+    sleep(0.25)
+    b.stop()
 
 def dispense(ml):
   print(ml + "ml dispensing")
@@ -37,5 +44,5 @@ def dispense(ml):
     endTones()
   print(ml + "ml dispense complete")
 
-
+button.wait_for_press()
 dispense(1000)
